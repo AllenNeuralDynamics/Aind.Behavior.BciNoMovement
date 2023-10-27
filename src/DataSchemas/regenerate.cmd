3cb02009dc5b92817cf97f36b8314e9e3c0c9c2f
bruno-f-cruz <bfpcruz@gmail.com>
2023-10-26 18:11:54 -0700

    Add rig schema

--- src/DataSchemas/regenerate.cmd
+++ src/DataSchemas/regenerate.cmd
@@ -1,1 +1,2 @@
 bonsai.sgen --schema "src\DataSchemas\bci-no-movement-logging.json" --namespace BciNoMovementDataSchema.Logging --root BciNoMovementLogging --output "src\Extensions\BciNoMovementLogging.cs" --serializer NewtonsoftJson YamlDotNet
+bonsai.sgen --schema "src\DataSchemas\bci-no-movement-rig.json" --namespace BciNoMovementDataSchema.Rig --root BciNoMovementRig --output "src\Extensions\BciNoMovementRig.cs" --serializer NewtonsoftJson YamlDotNet
