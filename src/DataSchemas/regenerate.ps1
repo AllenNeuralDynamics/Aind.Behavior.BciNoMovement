a412cd241d1a5615ca8104ae218e2a868a8d7c20
bruno-f-cruz <bfpcruz@gmail.com>
2023-12-05 13:49:04 -0800

    Update to use powershell

---
+++ src/DataSchemas/regenerate.ps1
@@ -0,0 +1,3 @@
+& bonsai.sgen --schema "src\DataSchemas\bci-no-movement-rig.json" --namespace BciNoMovementDataSchema.Rig --root BciNoMovementRig --output "src\Extensions\BciNoMovementRig.cs" --serializer NewtonsoftJson YamlDotNet
+& bonsai.sgen --schema "src\DataSchemas\bci-no-movement-session.json" --namespace BciNoMovementDataSchema.Session --root BciNoMovementSession --output "src\Extensions\BciNoMovementSession.cs" --serializer NewtonsoftJson YamlDotNet
+& bonsai.sgen --schema "src\DataSchemas\bci-no-movement-tasklogic.json" --namespace BciNoMovementDataSchema.TaskLogic --root BciNoMovementTaskLogic --output "src\Extensions\BciNoMovementTaskLogic.cs" --serializer NewtonsoftJson YamlDotNet
