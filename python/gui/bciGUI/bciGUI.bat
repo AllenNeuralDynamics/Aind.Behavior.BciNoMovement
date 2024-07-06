6602056868185f4eb883250dc5b62cf6ca7557e8
LKINSEY <lkinsey3@gmu.edu>
2024-07-05 22:51:57 -0700

    Created self contained GUI for BCI task that describes reaction time and mouse performance during task. Can load previous data for visualization, save mice, load settings/data of a previous mouse, and can save settings/data of specified mouse as json files in made directory. Publisher/Subscriber relationship with bonsai. GUI is subscriber, bonsai acts as publisher. Must subscribe to bonsai before pressing "start session" in bonsai gui. Everything is self contained in bat file, just run bat file and virtual environment will run everything necessary independent of repo or conda environment.

---
+++ python/gui/bciGUI/bciGUI.bat
@@ -0,0 +1,5 @@
+cd /d C:\git\AllenNeuralDynamics\Aind.Behavior.BciNoMovement\python\gui\bciGUI
+call environments\Scripts\activate
+python C:\git\AllenNeuralDynamics\Aind.Behavior.BciNoMovement\python\gui\bciGUI\bonsaiSessionLoader.py
+pause
+deactivate
