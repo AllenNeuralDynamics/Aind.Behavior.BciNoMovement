c289709d39572d65d4fee1217639f04eb0bfcacf
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-07-15 10:21:06 -0700

    Derive regeneration script from new schema

--- src/DataSchemas/regenerate.cmd
+++ src/DataSchemas/regenerate.cmd
@@ -1,1 +1,6 @@
-python src/DataSchemas/regenerate.py
+@echo off
+setlocal
+set "scriptPath=%~dp0"
+set "pythonScriptPath=%scriptPath%regenerate.ps1"
+powershell -ExecutionPolicy Bypass -File "%pythonScriptPath%"
+endlocal
--- src/DataSchemas/regenerate.py
+++ src/DataSchemas/regenerate.py
@@ -1,9 +1,10 @@
+import inspect
 from pathlib import Path
 
 import aind_bci_no_movement.rig
-import aind_bci_no_movement.session
 import aind_bci_no_movement.task_logic
-from aind_behavior_services.utils import convert_pydantic_to_bonsai
+from aind_behavior_services.session import AindBehaviorSessionModel
+from aind_behavior_services.utils import convert_pydantic_to_bonsai, pascal_to_snake_case, snake_to_pascal_case
 
 SCHEMA_ROOT = Path("./src/DataSchemas/")
 EXTENSIONS_ROOT = Path("./src/Extensions/")
@@ -11,14 +12,23 @@ NAMESPACE_PREFIX = "BciNoMovementDataSchema"
 
 
 def main():
-    models = {
-        "bci_no_movement_task": aind_bci_no_movement.task_logic.schema(),
-        "bci_no_movement_session": aind_bci_no_movement.session.schema(),
-        "bci_no_movement_rig": aind_bci_no_movement.rig.schema(),
-    }
-    convert_pydantic_to_bonsai(
-        models, schema_path=SCHEMA_ROOT, output_path=EXTENSIONS_ROOT, namespace_prefix=NAMESPACE_PREFIX
-    )
+
+    models = [
+        aind_bci_no_movement.task_logic.BciNoMovementTaskLogic,
+        aind_bci_no_movement.rig.BciNoMovementRig,
+        AindBehaviorSessionModel,
+    ]
+
+    for model in models:
+        module_name = inspect.getmodule(model).__name__
+        module_name = module_name.split(".")[-1]
+        schema_name = f"{pascal_to_snake_case(model.__name__)}"
+        namespace = f"{NAMESPACE_PREFIX}.{snake_to_pascal_case(module_name)}"
+
+        print((schema_name, namespace))
+        convert_pydantic_to_bonsai(
+            {schema_name: model}, schema_path=SCHEMA_ROOT, output_path=EXTENSIONS_ROOT, namespace=namespace
+        )
 
 
 if __name__ == "__main__":
